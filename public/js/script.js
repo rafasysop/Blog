496f2caa5ce10eea477df1f3fc6fb7dba39263fe
Rafael Moura <rafaelcostamoura@gmail.com>
2022-01-09 12:39:07 -0300

    bloqueando delete das minhas categorias/artigos

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,11 @@ function handleDelete(e, form) {
   var confirm = window.confirm('Deseja realmente Deletar?')
   if (confirm) return form.submit()
 }
+
+function handleEdit(e, form) {
+  e.preventDefault()
+  
+  const target = e.target[0].value <= 16
+  if(target) return alert('Você não pode editar esta categoria/artigo \n Crie um para poder editar')
+  return form.submit()
+}
